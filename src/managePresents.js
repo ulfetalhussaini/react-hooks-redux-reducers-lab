f79c48672c02e9a19416a23c74d46bc3a31eb889
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2021-08-29 23:59:25 +0300

    update

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,9 @@
 export function managePresents(state, action) {
   // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
 }
